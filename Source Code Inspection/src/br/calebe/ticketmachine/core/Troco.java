9ea822e0399c2dec5a90fd638a758f4a637fd692
41283554 <41283554@099247-P31S303.macklabs.br>
2014-10-08 21:46:46 -0300

    Correções da classe troco: Métodos: Troco; TrocoIterator;

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -6,42 +6,51 @@ import java.util.Iterator;
  *
  * @author Calebe de Paula Bianchini
  */
-class Troco {
+public class Troco {
 
     protected PapelMoeda[] papeisMoeda;
-
+    int valorAux;
+    
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
+        this.valorAux = valor;
+        
         int count = 0;
-        while (valor % 100 != 0) {
+        while (valorAux % 100 != 0) {
             count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
+        }papeisMoeda[5] = new PapelMoeda(100, count);
+        valorAux %= 100;
+        
+        
         count = 0;
-        while (valor % 50 != 0) {
+        while (valorAux % 50 != 0) {
             count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
+        }papeisMoeda[4] = new PapelMoeda(50, count);
+        valorAux %= 50;
+        
         count = 0;
-        while (valor % 20 != 0) {
+        while (valorAux % 20 != 0) {
             count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
+        }papeisMoeda[3] = new PapelMoeda(20, count);
+        valorAux %= 20;
+        
         count = 0;
-        while (valor % 10 != 0) {
+        while (valorAux % 10 != 0) {
             count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
+        }papeisMoeda[2] = new PapelMoeda(10, count);
+        valorAux %= 10;
+        
         count = 0;
-        while (valor % 5 != 0) {
+        while (valorAux % 5 != 0) {
             count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
+        }papeisMoeda[1] = new PapelMoeda(5, count);
+        valorAux %= 5;
+        
         count = 0;
-        while (valor % 2 != 0) {
+        while (valorAux % 2 != 0) {
             count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        }papeisMoeda[0] = new PapelMoeda(2, count);
+        //valorAux %= 2;
     }
 
     public Iterator<PapelMoeda> getIterator() {
@@ -58,7 +67,7 @@ class Troco {
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
+            for (int i = 5; i >= 0; i++) {
                 if (troco.papeisMoeda[i] != null) {
                     return true;
                 }
@@ -69,13 +78,14 @@ class Troco {
         @Override
         public PapelMoeda next() {
             PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
+            for (int i = 5; i >= 0 && ret != null; i++) {
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
+                    return ret;
                 }
             }
-            return ret;
+            return null;
         }
 
         @Override
